44b8375f8a686a0ce45eafcb9ec48b8328cbe72c
Laboratorio de Telecomunicaciones <cisco@academico.unab.ad>
2024-03-06 16:55:24 -0500

    Initial commit

---
+++ app/src/main/java/com/joserincon/tienda_virtual/FormProductActivity.java
@@ -0,0 +1,14 @@
+package com.joserincon.tienda_virtual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_product);
+    }
+}
